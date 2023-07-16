d891604312e4b78a4d9e0d7f2e609bad634d4b1d
tannd-ds <donhattan1012dev@gmail.com>
2023-07-16 09:30:16 +0700

    remove read table from github

--- app.vue
+++ app.vue
@@ -10,25 +10,7 @@
   import { useAnnotationInputStore } from "~/stores/annotationInput"
   const aInput = useAnnotationInputStore()
 
-  onMounted(async () => {
-    aInput.loadConfirmedData()
-
-    // Load tables saved on Github :)
-    let github_data = await $fetch('https://raw.githubusercontent.com/tannd-ds/vi-wiki-table-qa/main/test.json')
-    github_data = JSON.parse(github_data)
-    let new_table = []
-    for (let row_id in github_data) {
-      let new_row = []
-      for (let cell_id in github_data[row_id]) {
-        new_row.push(github_data[row_id][cell_id])
-      }
-      new_table.push(new_row)
-    }
-    aInput.tables.push({
-      'id': '0003',
-      'content': new_table
-    })
-  })
+  aInput.loadConfirmedData()
 </script>
 
 <style>
