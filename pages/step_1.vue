793d76f26e17429394d5382f712d27cb6c14e50d
tannd-ds <donhattan1012dev@gmail.com>
2023-07-17 08:31:35 +0700

    Change "Hint" design

--- pages/step_1.vue
+++ pages/step_1.vue
@@ -8,34 +8,30 @@
     </Head>
     <div class="w-full flex flex-col gap-8 justify-center items-center lg:flex-row">
       <div class="w-full max-w-lg">
-        <BaseBox :override_bg="true">
-          <div 
-            class="
-              w-full min-h-[20vh] p-4
-              bg-gradient-to-tl from-emerald-950 to-green-800
-              rounded-2xl
-              text-gray-100
-              flex flex-col
-            "
-          >
-            <div class="flex gap-4 items-center">
-              <BaseBox 
-                :override_bg="true"
-                class="p-4 text-yellow-600"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
-                </svg>
-
-              </BaseBox>
-              Hint!
+        <BaseBox :override_bg="true" class="border-2 border-yellow-500">
+          <div class="w-full h-full relative">
+            <div 
+              class="
+                max-h-[30%] w-full max-w-[35%] p-4 
+                absolute -top-8 left-8 
+                flex justify-center items-center gap-4 
+                bg-yellow-500
+                rounded-2xl text-midnight-200 font-bold
+              "
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-8 h-8 rotate-[39deg]">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
+              </svg>
+              <div class="text-2xl">
+                Hint!
+              </div>
             </div>
-            <div class="px-4 pb-2 leading-loose">
-              <p>Prompt của bạn PHẢI THOẢ 1 TRONG CÁC YÊU CẦU sau đây</p>
-              <ul class="pl-5 text-gray-200 list-item">
-                <li>Yêu cầu thứ 1</li>
-                <li>Yêu cầu thứ 2</li>
-                <li>Yêu cầu thứ 3</li>
+            <div class="mt-8 p-4 dark:bg-midnight-100 rounded-2xl dark:text-gray-300 leading-8">
+              <p class="font-bold text-center">Your QA should meet ONE OF THESE CRITERIA</p>
+              <ul class="pl-12 list-disc">
+                <li>This is Criteria 1.</li>
+                <li>Like above, but this is Criteria 2. I make this longer for experimental purpose.</li>
+                <li>And this is Criteria 3, of course.</li>
               </ul>
             </div>
           </div>
@@ -94,7 +90,5 @@
     const redirect_to = general_store.check_step(route.name)
     if (redirect_to != route.name) router.push(redirect_to)
   })
-
-  console.log(aInput.getCurrentTableHTML)
 
 </script>
