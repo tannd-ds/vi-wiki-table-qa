7f928bbf723210b24064ccbbf6ce41ad0f0e8529
tannd-ds <donhattan1012dev@gmail.com>
2023-07-21 15:19:00 +0700

    change hint's checkbox design

--- pages/step_1.vue
+++ pages/step_1.vue
@@ -65,7 +65,7 @@
                       }"
                     >
                       <Transition name="fade">
-                        <svg v-if="hint.is_checked" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="6" stroke="currentColor" class="w-3 h-3 dark:text-green-900">
+                        <svg v-if="hint.is_checked" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="6" stroke="currentColor" class="w-2 h-2 dark:text-green-900">
                           <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
                         </svg>
                       </Transition>
