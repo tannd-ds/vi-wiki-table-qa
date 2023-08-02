f65df9b5acef7859610a79ad0eefdbe3dee83165
tannd-ds <donhattan1012dev@gmail.com>
2023-08-02 22:52:13 +0700

    make label slide up on focus

--- components/InputField.vue
+++ components/InputField.vue
@@ -1,9 +1,6 @@
 <template>
     <div>
-      <div class="mb-6">
-        <label class="block text-gray-700 text-sm font-bold mb-2 dark:text-white" for="password">
-          {{ label_name }}
-        </label>
+      <div class="mt-2 mb-6 relative">
         <component
           :is="is" 
           class="
@@ -11,19 +8,31 @@
             w-full p-3
             text-gray-700 leading-tight focus:outline-green-400 
             dark:bg-midnight-200 dark:border-gray-500 dark:text-gray-100
+            peer
           " 
           :id="id" 
           :type="type" 
           :value="modelValue"
           :rows="is == `textarea` ? 10 : 1"
           @input="$emit('update:modelValue', $event.target.value)"
-          :placeholder="placeholder"
           spellcheck="false" 
           autocomplete="off" 
           aria-autocomplete="none"
         >
           <slot></slot>
         </component>
+        <label 
+          class="
+            px-2
+            absolute top-3 left-2 peer-focus:-top-3 peer-focus:bg-midnight-100
+            block 
+            text-gray-700 text-sm font-bold mb-2 dark:text-gray-400 peer-focus:text-gray-200
+            transition-all
+          " 
+          :for="id"
+        >
+          {{ label_name }}
+        </label>
       </div>
     </div>
 </template>
