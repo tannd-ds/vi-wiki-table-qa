c6823dc2b03f5ade4db7a9c60fb477ec1c5c66ac
tannd-ds <donhattan1012dev@gmail.com>
2023-08-08 21:31:12 +0700

    des: colorful navbar btn

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -82,10 +82,16 @@
           <div class="relative flex gap-2">
             <button 
                 :disabled="aInput.confirmedData.length <= 0"
-                class="p-1 relative rounded-lg hover:bg-green-200 text-black dark:text-white dark:hover:bg-green-500"
+                class="
+                  p-2 relative 
+                  rounded-lg 
+                  bg-green-200 dark:bg-green-400
+                  dark:bg-opacity-80 dark:hover:bg-opacity-70
+                  text-green-500 dark:text-green-200
+                "
                 @click="general_store.show_overlay('confirmed_list')"
               >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a2.25 2.25 0 00-2.25-2.25H15a3 3 0 11-6 0H5.25A2.25 2.25 0 003 12m18 0v6a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 9m18 0V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v3" />
               </svg>  
               <div class="absolute inline-flex items-center justify-center w-7 h-7 text-xs font-bold text-white bg-green-500 border-2 border-white rounded-full -top-1 -right-2 dark:border-gray-900" >
@@ -109,9 +115,15 @@
           <!-- User -->
           <div class="relative flex gap-2">
             <button 
-              class="p-1 relative rounded-lg hover:bg-green-200 text-black dark:text-white dark:hover:bg-green-500" 
+              class="
+                p-2 relative 
+                rounded-lg 
+                bg-orange-200 dark:bg-orange-400
+                dark:bg-opacity-80 dark:hover:bg-opacity-70
+                text-orange-500 dark:text-orange-200
+              " 
             >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
               </svg>
             </button>
@@ -144,9 +156,16 @@
           <!-- Color Mode -->
           <div class="relative flex gap-2">
             <button 
-                @click="general_store.use_darkmode = !general_store.use_darkmode" 
-                class="p-1 relative rounded-lg hover:bg-green-200 text-black dark:text-white dark:hover:bg-green-500" >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+              @click="general_store.use_darkmode = !general_store.use_darkmode" 
+              class="
+                p-2 relative 
+                rounded-lg 
+                bg-purple-200 dark:bg-purple-400
+                dark:bg-opacity-80 dark:hover:bg-opacity-70
+                text-purple-500 dark:text-purple-200
+              " 
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
               </svg>
             </button>
