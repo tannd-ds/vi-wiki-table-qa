ff9cddaab80c4f27126b6e42bb966d7d43be8a46
tannd-ds <donhattan1012dev@gmail.com>
2023-08-16 21:40:19 +0700

    bugfix: click on "checkbox" to check

--- components/Hint.vue
+++ components/Hint.vue
@@ -50,6 +50,7 @@
               />
               <div
                 class="absolute flex h-4 w-4 items-center justify-center opacity-0 transition-all peer-checked:opacity-100"
+                @click="hint.is_checked = !hint.is_checked"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
