c74c4f5bfdb0fc3074cf6ad5b66e6e0f7dd7eeef
tannd-ds <donhattan1012dev@gmail.com>
2023-08-22 01:50:35 +0700

    des:minor change Overlay for smooth transition

--- components/AppOverlay.vue
+++ components/AppOverlay.vue
@@ -1,15 +1,29 @@
 <template>
-  <Transition name="fade" is="div">
-    <div
-      v-if="general_store.overlayIsShow"
-      class="absolute left-0 top-0 z-20 flex h-full min-h-screen w-screen items-baseline justify-center backdrop-blur-xl lg:items-center"
-      @keydown.ctrl="console.log('Yos')"
+  <div>
+    <Transition name="fade" is="div">
+      <div
+        v-if="general_store.overlayIsShow"
+        class="absolute left-0 top-0 z-20 flex h-full min-h-screen w-screen items-baseline justify-center backdrop-blur-xl delay-1000 lg:items-center"
+      ></div>
+    </Transition>
+    <transition
+      enter-active-class="transition duration-500 ease-out"
+      enter-from-class="translate-y-1 opacity-0"
+      enter-to-class="translate-y-0 opacity-100"
+      leave-active-class="transition duration-200 ease-in"
+      leave-from-class="translate-y-0 opacity-100"
+      leave-to-class="translate-y-1 opacity-0"
     >
-      <div ref="modal">
-        <component :is="currentOverlay"></component>
-      </div>
-    </div>
-  </Transition>
+      <KeepAlive>
+        <div
+          v-if="general_store.overlayIsShow"
+          class="absolute left-0 top-0 z-[21] flex h-full min-h-screen w-screen items-baseline justify-center backdrop-blur-xl lg:items-center"
+        >
+          <component :is="currentOverlay" ref="modal"></component>
+        </div>
+      </KeepAlive>
+    </transition>
+  </div>
 </template>
 
 <script setup>
